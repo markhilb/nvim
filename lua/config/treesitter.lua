051274b1cf432fa273da44acf798a78cd10b9362
Markus Bergum Hilbertsen <markus@hilbertsen.com>
2021-11-30 21:58:28 +0100

    Add default treesitter highlights

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -2,6 +2,19 @@ vim.g.OmniSharp_highlighting = 0
 
 require("nvim-treesitter.configs").setup(
   {
+    ensure_installed = {
+      "bash",
+      "c",
+      "c_sharp",
+      "css",
+      "dockerfile",
+      "html",
+      "javascript",
+      "json",
+      "lua",
+      "python",
+      "scss"
+    },
     highlight = {
       enable = true,
       disable = {"typescript"},
