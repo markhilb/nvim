5360fbbb51b58a0fdb9ffdf794d2e2fdc50fa294
Markus Bergum Hilbertsen <markus@hilbertsen.com>
2021-12-10 21:28:06 +0100

    Noconfirm

--- lsp_install.sh
+++ lsp_install.sh
@@ -1,3 +1,3 @@
 sudo npm i -g pyright typescript typescript-language-server vscode-langservers-extracted
-sudo pacman -S clang # (https://clangd.llvm.org/installation.html)
+sudo pacman -S --noconfirm clang # (https://clangd.llvm.org/installation.html)
 curl -L https://github.com/OmniSharp/omnisharp-roslyn/releases/latest/download/omnisharp-linux-x64.tar.gz > ~/omnisharp.tar.gz && tar xzf ~/omnisharp.tar.gz -C ~/.omnisharp/ && rm ~/omnisharp.tar.gz
