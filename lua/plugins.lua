dcfda73e886544e1c860d53b44533ee847059315
Markus Bergum Hilbertsen <markus@hilbertsen.com>
2021-12-15 13:18:16 +0100

    cleanup

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -2,7 +2,7 @@ local execute = vim.api.nvim_command
 local install_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
   execute("!git clone https://github.com/wbthomason/packer.nvim " .. install_path)
-  execute "packadd packer.nvim"
+  execute("packadd packer.nvim")
 end
 vim.cmd("packadd packer.nvim")
 
@@ -59,11 +59,5 @@ return require("packer").startup(
         {"L3MON4D3/LuaSnip", config = "require('config/luasnip')"}
       }
     }
-
-    -- Automatically set up your configuration after cloning packer.nvim
-    -- Put this at the end after all plugins
-    if packer_bootstrap then
-      require("packer").sync()
-    end
   end
 )
