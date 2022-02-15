440ac9a2d8f244e636b521353074b988a92cead1
Markus Bergum Hilbertsen <markus@hilbertsen.com>
2022-02-15 13:13:30 +0100

    Improve lsp install script

--- lsp_install.sh
+++ lsp_install.sh
@@ -1,3 +1,36 @@
-sudo npm i -g pyright typescript typescript-language-server vscode-langservers-extracted
-sudo pacman -S --noconfirm clang # (https://clangd.llvm.org/installation.html)
-curl -L https://github.com/OmniSharp/omnisharp-roslyn/releases/latest/download/omnisharp-linux-x64.tar.gz > ~/omnisharp.tar.gz && tar xzf ~/omnisharp.tar.gz -C ~/.omnisharp/ && rm ~/omnisharp.tar.gz
+evaluate() {
+    [ -z $1 ] || [ "$1" == "Y" ] || [ "$1" == "y" ] || [ "$1" == "yes" ]
+}
+
+COMMANDS=()
+
+
+printf "Do you want to install python lsp? [Y/n] "
+read ANSWER
+if evaluate $ANSWER; then
+    COMMANDS+=("sudo npm i -g pyright")
+fi
+
+printf "\nDo you want to install typescript lsp? [Y/n] "
+read ANSWER
+if evaluate $ANSWER; then
+    COMMANDS+=("sudo npm i -g typescript typescript-language-server vscode-langservers-extracted")
+fi
+
+printf "\nDo you want to install C/C++ lsp? [Y/n] "
+read ANSWER
+if evaluate $ANSWER; then
+    COMMANDS+=("sudo pacman -S --noconfirm clang")
+fi
+
+printf "\nDo you want to install .NET lsp? [Y/n] "
+read ANSWER
+if evaluate $ANSWER; then
+    COMMANDS+=("[ ! -d ~/.omnisharp ] && mkdir .omnisharp/")
+    COMMANDS+=("curl -L https://github.com/OmniSharp/omnisharp-roslyn/releases/latest/download/omnisharp-linux-x64.tar.gz > ~/omnisharp.tar.gz && tar xzf ~/omnisharp.tar.gz -C ~/.omnisharp/ && rm ~/omnisharp.tar.gz")
+fi
+
+for cmd in "${COMMANDS[@]}"
+do
+    eval $cmd
+done
