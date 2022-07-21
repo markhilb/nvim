4b72f702876024a0978d4f4393fcd00456e824f3
Markus Bergum Hilbertsen <markus@orcalabs.no>
2022-07-21 11:03:57 +0200

    make ~/.omnisharp if not exist

--- lsp_install.sh
+++ lsp_install.sh
@@ -21,7 +21,7 @@ install "typescript lsp" "npm i -g typescript typescript-language-server vscode-
 
 install "C/C++ lsp" "sudo pacman -S --noconfirm clang"
 
-install ".NET lsp" "[ ! -d ~/.omnisharp ] && mkdir .omnisharp/" "curl -L https://github.com/OmniSharp/omnisharp-roslyn/releases/latest/download/omnisharp-linux-x64-net6.0.tar.gz > ~/omnisharp.tar.gz && tar xzf ~/omnisharp.tar.gz -C ~/.omnisharp/ && rm ~/omnisharp.tar.gz"
+install ".NET lsp" "mkdir -p ~/.omnisharp && curl -L https://github.com/OmniSharp/omnisharp-roslyn/releases/latest/download/omnisharp-linux-x64-net6.0.tar.gz > ~/omnisharp.tar.gz && tar xzf ~/omnisharp.tar.gz -C ~/.omnisharp/ && rm ~/omnisharp.tar.gz"
 
 install "Rust lsp" "mkdir -p ~/.local/bin && curl -L https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-x86_64-unknown-linux-gnu.gz | gunzip -c - > ~/.local/bin/rust-analyzer && chmod +x ~/.local/bin/rust-analyzer"
 
