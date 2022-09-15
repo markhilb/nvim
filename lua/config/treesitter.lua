58ec6b23235919ea4b3159253956792d569ce1aa
Markus Bergum Hilbertsen <markus@orcalabs.no>
2022-09-15 13:46:59 +0200

    update colorscheme & use treesitter for all files

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -1,26 +1,7 @@
 require("nvim-treesitter.configs").setup(
   {
-    ensure_installed = {
-      "c",
-      "c_sharp",
-      "comment",
-      "dockerfile",
-      "javascript",
-      "typescript",
-      "tsx",
-      "html",
-      "css",
-      "scss",
-      "json",
-      "markdown",
-      "lua",
-      "python",
-      "rust"
-    },
-    highlight = {
-      enable = true,
-      disable = {"typescript", "tsx", "bash", "html", "css", "scss", "rust"}
-    },
+    ensure_installed = "all",
+    highlight = {enable = true},
     autotag = {enable = true}
   }
 )
