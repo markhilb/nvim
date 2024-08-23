42db0fbb9e7f7856b454e2e37e0b83d5e3c3cf5a
Markus Bergum Hilbertsen <markus@orcalabs.no>
2024-08-23 12:43:37 +0200

    try out lazydocker

---
+++ lua/plugins/lazydocker.lua
@@ -0,0 +1,11 @@
+return {
+    'crnvl96/lazydocker.nvim',
+    event = 'VeryLazy',
+    opts = {},
+    dependencies = {
+        'MunifTanjim/nui.nvim',
+    },
+    keys = {
+        { '<leader>x', '<cmd>LazyDocker<CR>', desc = 'LazyDocker' },
+    },
+}
