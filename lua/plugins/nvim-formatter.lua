d09ff60f8aab5a4396d5264d557770446be35d79
Markus Bergum Hilbertsen <markus@orcalabs.no>
2024-09-12 08:57:27 +0200

    fix lsp format

--- lua/plugins/nvim-formatter.lua
+++ lua/plugins/nvim-formatter.lua
@@ -1,40 +1,47 @@
+local filetype = {
+    javascript = 'prettierd .js',
+    typescript = 'prettierd .ts',
+    typescriptreact = 'prettierd .tsx',
+    javascriptreact = 'prettierd .jsx',
+    html = 'prettierd .html',
+    css = 'prettierd .css',
+    scss = 'prettierd .scss',
+    json = 'prettierd .json',
+    markdown = 'prettierd .md',
+    yaml = 'prettierd .yaml',
+    rust = 'rustfmt --edition=2021',
+    lua = 'stylua --search-parent-directories -',
+    python = 'autopep8 -',
+    c = 'clang-format',
+    sql = function()
+        local config = require('config.utils').reverse_find_file('.sql-formatter.json')
+        return {
+            exe = 'sql-formatter',
+            args = config == nil and { '-l', 'postgresql' } or { '--config', config },
+        }
+    end,
+}
+
 return {
     'seblj/nvim-formatter',
     keys = {
         {
             '<leader>a',
             function()
-                vim.cmd('w')
-                vim.cmd.Format()
+                if filetype[vim.bo.filetype] == nil then
+                    vim.lsp.buf.format()
+                    vim.cmd('w')
+                else
+                    vim.cmd('w')
+                    vim.cmd.Format()
+                end
             end,
             desc = 'Format buffer',
         },
     },
     config = function()
         require('formatter').setup({
-            filetype = {
-                javascript = 'prettierd .js',
-                typescript = 'prettierd .ts',
-                typescriptreact = 'prettierd .tsx',
-                javascriptreact = 'prettierd .jsx',
-                html = 'prettierd .html',
-                css = 'prettierd .css',
-                scss = 'prettierd .scss',
-                json = 'prettierd .json',
-                markdown = 'prettierd .md',
-                yaml = 'prettierd .yaml',
-                rust = 'rustfmt --edition=2021',
-                lua = 'stylua --search-parent-directories -',
-                python = 'autopep8 -',
-                c = 'clang-format',
-                sql = function()
-                    local config = require('config.utils').reverse_find_file('.sql-formatter.json')
-                    return {
-                        exe = 'sql-formatter',
-                        args = config == nil and { '-l', 'postgresql' } or { '--config', config },
-                    }
-                end,
-            },
+            filetype = filetype,
         })
     end,
 }
