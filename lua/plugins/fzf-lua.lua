f89cb8e308adf467e62c47af8cc723954c45c8d8
Markus Bergum Hilbertsen <markus@orcalabs.no>
2025-02-03 10:09:51 +0100

    add lsp symbols keybind

--- lua/plugins/fzf-lua.lua
+++ lua/plugins/fzf-lua.lua
@@ -34,7 +34,8 @@ return {
         vim.keymap.set('n', '<leader>td', function() fzf_lua.files({ cwd = '~/dotfiles' }) end)
         vim.keymap.set('n', '<leader>tb', fzf_lua.buffers)
         vim.keymap.set('n', '<leader>tr', fzf_lua.command_history)
-        vim.keymap.set('n', '<leader>ts', fzf_lua.search_history)
+        vim.keymap.set('n', '<leader>ts', fzf_lua.lsp_document_symbols)
+        vim.keymap.set('n', '<leader>tS', fzf_lua.lsp_workspace_symbols)
         vim.keymap.set('n', '<leader>tc', fzf_lua.git_commits)
     end,
 }
